7fc609fef760ca96b0ea149e98cde2c4a5a1612a
GainDev <DmitryChebotaru@GoITAdmins-iMac-7.local>
2016-11-22 18:31:49 +0200

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,129 @@
+//Форма отправки 2.0
+$(function() {
+  $("[name=send]").click(function () {
+    $(":input.error").removeClass('error');
+    $(".allert").remove();
+
+    var error;
+    var btn = $(this);
+    var ref = btn.closest('form').find('[required]');
+    var msg = btn.closest('form').find('input, textarea, select');
+    var send_btn = btn.closest('form').find('[name=send]');
+    var send_options = btn.closest('form').find('[name=campaign_token]');
+
+
+
+    $(ref).each(function() {
+      if ($(this).val() == '') {
+        var errorfield = $(this);
+        $(this).addClass('error').parent('.field').append('<div class="allert"><span>Заполните это поле</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
+        error = 1;
+        $(":input.error:first").focus();
+        return;
+      } else {
+        var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+        if ($(this).attr("type") == 'email') {
+          if(!pattern.test($(this).val())) {
+            $("[name=email]").val('');
+            $(this).addClass('error').parent('.field').append('<div class="allert"><span>Укажите коректный e-mail</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
+            error = 1;
+            $(":input.error:first").focus();
+          }
+        }
+        var patterntel = /^()[- +()0-9]{9,18}/i;
+        if ( $(this).attr("type") == 'tel') {
+          if(!patterntel.test($(this).val())) {
+            $("[name=phone]").val('');
+            $(this).addClass('error').parent('.field').append('<div class="allert"><span>Укажите номер телефона в формате +3809999999</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
+            error = 1;
+            $(":input.error:first").focus();
+          }
+        }
+      }
+    });
+    if(!(error==1)) {
+      $(send_btn).each(function() {
+        $(this).attr('disabled', true);
+      });
+      $(send_options).each(function() {
+        var form = $(this).closest('form'), name = form.find('.name').val();
+        if ($(this).val() == '') {
+          $.ajax({
+            type: 'POST',
+            url: 'mail.php',
+            data: msg,
+            success: function() {
+              $('form').trigger("reset");
+              setTimeout(function(){  $("[name=send]").removeAttr("disabled"); }, 1000);
+                    // Настройки модального окна после удачной отправки
+                        $('div.md-show').removeClass('md-show');
+                        $('form').trigger("reset");
+                        $("#call_ok")[0].click();
+                  },
+                  error: function(xhr, str) {
+                    alert('Возникла ошибка: ' + xhr.responseCode);
+                  }
+                });
+          } else {
+          $.ajax({
+            type: 'POST',
+            url: 'mail.php',
+            data: msg,
+            success:
+            $.ajax({
+              type: 'POST',
+              url: 'https://app.getresponse.com/add_subscriber.html',
+              data: msg,
+              statusCode: {0:function() {
+                $( "#modal_callback_ok h4" ).remove();
+                $( "#modal_callback_ok" ).prepend("<h4>"+name+",</h4>");
+                $('form').trigger("reset");
+                setTimeout(function(){  $("[name=send]").removeAttr("disabled"); }, 1000);
+                // Настройки модального окна после удачной отправки
+                $('div.md-show').removeClass('md-show');
+                $('form').trigger("reset");
+                $("#call_ok")[0].click();
+              }}
+            }),
+            error:  function(xhr, str) {
+              alert('Возникла ошибка: ' + xhr.responseCode);
+            }
+          });
+        }
+      });
+    }
+    return false;
+  })
+});
+
+
+
+
+
+ // Smooth scroll to anchor
+
+ $('.scroll').click(function(){
+  $('html, body').animate({
+    scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 1000);
+  return false;
+});
+
+//  INPUT TEL MASK
+
+jQuery(function($){
+ $("input[type='tel']").mask("+9 (999) 999-9999");
+});
+
+
+
+// Scroll BAR
+
+$(window).scroll(function() {
+    // calculate the percentage the user has scrolled down the page
+    var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
+
+    $('.bar-long').css('width', scrollPercent +"%"  );
+
+  });
+
