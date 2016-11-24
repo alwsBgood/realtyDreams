7f878cfdcb37bfda6477ff96dba9d0956c124d62
Дмитрий Чеботару < >
2016-11-24 10:58:42 +0200

    modalsAdd

--- app/js/common.js
+++ app/js/common.js
@@ -188,46 +188,29 @@ $(document).ready(function() {
     });
 });
 
-
-$('body').each(function() {
-    var body = $(this);
-    var img_url = $(this).data('img');
-    var img = new Image();
-    img.src = img_url;
-    img.onload = function(){
-        var ppbox = '<div id="pp" style="background: url('+img_url+') no-repeat 50% 0%;top:0;width:100%;position:absolute;z-index:1000000;opacity:0.5;height:'+img.height+'px"></div>';
-        var ppbtn = '<button onclick="myOff()" id="ppbtn" style="position:fixed;top:0;right:0;z-index:1000001">ON</button>'
-        body.append(ppbox);
-        body.append(ppbtn);
-    };
+$(document).ready(function(){
+ $('.slider-big').slick({
+  slidesToShow: 1,
+  dots: false,
+  arrows: false,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-small'
 });
-function myOff() {
-    var ppbtntext = $('#ppbtn').text();
-    if (ppbtntext == 'ON') {
-        $('#ppbtn').text('OFF');
-        $('#pp').css('display', 'none');
-    } else {
-        $('#ppbtn').text('ON');
-        $('#pp')        .css({
-          ' z-index' : '1000000',
-          display: 'block'
-        });
+ });
 
-    }
-}
+$(document).ready(function(){
+$('.slider-small').slick({
+  slidesToShow: 3,
+  dots: false,
+  arrows: false,
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToScroll: 1,
+  asNavFor: '.slider-big',
+  focusOnSelect: true,
 
-$('html').keydown(function(){
-  var ppbtntext = $('#ppbtn').text();
-  if (event.keyCode == 81) {
-    if (ppbtntext == 'ON') {
-        $('#ppbtn').text('OFF');
-        $('#pp').css('display', 'none');
-    } else {
-        $('#ppbtn').text('ON');
-        $('#pp')        .css({
-          ' z-index' : '1000000',
-          display: 'block'
-        });
-    }
-  }
+});
 });
